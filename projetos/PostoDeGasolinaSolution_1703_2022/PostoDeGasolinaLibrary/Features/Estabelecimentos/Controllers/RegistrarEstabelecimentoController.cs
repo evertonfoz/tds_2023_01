8d7e5e355dc56e60cadc42012cb9e3d8419f6183
Everton Coimbra de Araújo <evertoncoimbra@gmail.com>
2022-03-15 09:17:20 -0300

    0917

---
+++ projetos/PostoDeGasolinaSolution_1703_2022/PostoDeGasolinaLibrary/Features/Estabelecimentos/Controllers/RegistrarEstabelecimentoController.cs
@@ -0,0 +1,20 @@
+﻿using PostoDeGasolinaLibrary.Estabelecimentos.Data.DataSources.Implementations;
+using PostoDeGasolinaLibrary.Estabelecimentos.Data.Repositories.Implementations;
+using PostoDeGasolinaLibrary.Estabelecimentos.Domain.Entities;
+using PostoDeGasolinaLibrary.Estabelecimentos.Domain.UseCases;
+
+namespace PostoDeGasolinaLibrary.Estabelecimentos.Controllers
+{
+    public class RegistrarEstabelecimentoController
+    {
+        public async Task callable(EstabelecimentoEntity estabelecimentoEntity)
+        {
+            var _useCase = new RegistrarEstabelecimentoUseCase(
+                new RegistrarEstabelecimentoRepositorySQLServerImplementation
+                (new RegistrarEstabelecimentoDSSqlServerImplementation())
+                );
+
+            await _useCase.callable(estabelecimentoEntity);
+        }
+    }
+}

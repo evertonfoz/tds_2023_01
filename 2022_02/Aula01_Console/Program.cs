bbade1ce186f4b5a03a40291272f7ee880e9f0bd
Everton Coimbra de Araújo <evertoncoimbra@gmail.com>
2022-08-17 10:55:01 -0300

    2022_02

---
+++ 2022_02/Aula01_Console/Program.cs
@@ -0,0 +1,15 @@
+﻿HttpClient client = new();
+client.BaseAddress = new Uri("http://localhost:5073");
+
+
+    using (HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "/weatherforecast"))
+    {
+        using (HttpResponseMessage response = await client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead).ConfigureAwait(false))
+        {
+            response.EnsureSuccessStatusCode();
+             var teste = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+        Console.WriteLine(teste);
+        }
+    }
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");

731ca9192edebe884e092774fd663488d5594303
Everton Coimbra de Araújo <evertoncoimbra@gmail.com>
2022-09-13 13:54:35 -0300

    Aula de 12092022

---
+++ 2022_02/Aula_UmParaMuitos/Modelos/Marca.cs
@@ -0,0 +1,20 @@
+﻿namespace Aula_UmParaMuitos.Modelos
+{
+    internal class Marca
+    {
+        public int MarcaID { get; set; }
+        public string Nome { get; set; }
+        public Dictionary<int, Modelo> Modelos { get; private set; } = new();
+
+        public void RegistrarModelo(Modelo modelo)
+        {
+            Modelos.Add(modelo.ModeloID, modelo);
+            //if (!Modelos.Contains(modelo)) {
+            //    Modelos.Add(modelo);
+            //} else
+            //{
+            //    throw new Exception($"{modelo.Nome} já está registrado");
+            //}
+        }
+    }
+}
